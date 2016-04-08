c0bd9ceb39a3e7fc6c65a3f0e810897b144c93a4
CONSOLNY <proslike@gmail.com>
2016-04-08 10:31:28 +0600

    Changed drop method to take items by id from inventory list

--- first_app/views/drop.py
+++ first_app/views/drop.py
@@ -8,23 +8,26 @@ from first_app.models import *
 from first_app.views.draw_world import draw_world
 
 def drop(request):
+    item_id = int(request.POST.get("Inventory", -1))
+    if item_id == -1:
+        return draw_world(request)
+
     char = Character.objects.first()
-    can_drop_item = InventoryCharacter.objects.filter(char=char).exists()
-    if can_drop_item:
-        item = InventoryCharacter.objects.filter(char=char).first()
-        new_cell = InventoryCell.objects.create(inv_coord=char.cell, inv_content=item.content)
-        slot = SlotCharacter.objects.get(slot=item)
-        if slot.slot_type.item == 'Weapon':
-            attack_stats = slot.slot.content.item_stats
+    item_for_drop = InventoryCharacter.objects.get(char=char, id=item_id)
+    new_cell = InventoryCell.objects.create(inv_coord=char.cell, inv_content=item_for_drop.content)
+    slot_for_drop_exists = SlotCharacter.objects.filter(slot=item_for_drop).exists()
+    if slot_for_drop_exists:
+        slot_for_drop = SlotCharacter.objects.get(slot=item_for_drop)
+        if slot_for_drop.slot_type.item == 'Weapon':
+            attack_stats = slot_for_drop.slot.content.item_stats
             char.attack -= attack_stats
-            slot.slot = None
-            slot.save()
-            char.save()
-        elif slot.slot_type.item == 'Armor':
-            defense_stats = slot.slot.content.item_stats
+            slot_for_drop.slot = None
+            slot_for_drop.save()
+        elif slot_for_drop.slot_type.item == 'Armor':
+            defense_stats = slot_for_drop.slot.content.item_stats
             char.defense -= defense_stats
-            slot.slot = None
-            slot.save()
-            char.save()
-    item.delete()
+            slot_for_drop.slot = None
+            slot_for_drop.save()
+    char.save()
+    item_for_drop.delete()
     return draw_world(request)
